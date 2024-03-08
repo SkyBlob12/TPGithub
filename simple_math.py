5125448674fb25e4ca40e09fd62b43592a206cc5
SkyBlob12 <clement.andrieu12850@gmail.com>
2024-03-08 16:05:59 +0100

    add soustraction

--- simple_math.py
+++ simple_math.py
@@ -5,9 +5,16 @@ class SimpleMath:
     def addition(a, b):
         return a + b
 
+    @staticmethod
+    def soustraction(a, b):
+        return a - b
+
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(SimpleMath.addition(2, 3) , 5)
+
+    def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(7, 4), (3))
 
 if __name__== "__main__":
     unittest.main()
