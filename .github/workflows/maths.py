f08d30b78191d97b286a04e7ad48fb11eaa8889d
SkyBlob12 <clement.andrieu12850@gmail.com>
2024-03-08 15:49:15 +0100

    modifie maths + yaml

--- .github/workflows/maths.py
+++ .github/workflows/maths.py
@@ -1,11 +1,13 @@
+import unittest
+
 class SimpleMath:
     @staticmethod
     def addition(a, b):
         return a + b
 
-    @staticmethod
-    def soustraction(a, b):
-        return a - b
+class TestSimpleMath(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(SimpleMath.addition(2, 3) , 5)
 
-if __name__ == '__main__':
+if __name__== "__main__":
     unittest.main()
